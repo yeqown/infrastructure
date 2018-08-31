1ffd6a1de9147fb9e6ff1c05f9de1248e1587dc7
yeqown <yeqown@gmail.com>
2018-08-31 12:45:22 +0800

    feat: 更优雅的解析结构体的方式

--- dbs/tools/parser.go
+++ dbs/tools/parser.go
@@ -2,6 +2,7 @@ package tools
 
 // 解析一个go文件获取其中之类特征的struct并解析结构
 import (
+	"go/types"
 	"log"
 	"path"
 	"strings"
@@ -21,36 +22,43 @@ var (
 
 // UsageCfg ... config tools some feature
 type UsageCfg struct {
-	ExportDir          string
-	ExportFilename     string
-	ExportPkgName      string
+	// Dir 设置需要对那一路径下的文件进行解析
+	Dir string
+	// ExportDir 设置生成的文件的存放地址
+	ExportDir string
+	// ExportFilename 指定生成的文件名字
+	ExportFilename string
+	// ExportPkgName 指定生成的文件的包名
+	ExportPkgName string
+	// ExportStructSuffix 指定生成的新的机构体后缀
 	ExportStructSuffix string
-	ModelImportPath    string
-	StructSuffix       string
-	Debug              bool
+	// ModelImportPath 指定源文件所在包的导入路径
+	ModelImportPath string
+	// StructSuffix 需要解析的自定义结构体后缀
+	StructSuffix string
+	// Debug 调试模式开关
+	Debug bool
+	// Filenames 指定需要解析的.go源文件 文件名字
+	Filenames []string
 }
 
 // ParseAndGenerate parse all input go files and
 // get wanted struct info save with innerStruct, and then generate file
-func ParseAndGenerate(uCfg *UsageCfg, dir string, filenames ...string) error {
-	specifiedStructTypeSuffix = uCfg.StructSuffix
-	isDebug = uCfg.Debug
+func ParseAndGenerate(cfg *UsageCfg) error {
+	specifiedStructTypeSuffix = cfg.StructSuffix
+	isDebug = cfg.Debug
 
 	// parse
-	ises, err := loadGoFiles(dir, filenames...)
+	ises, err := loadGoFiles(cfg.Dir, cfg.ModelImportPath, cfg.Filenames...)
 	if err != nil {
 		return err
 	}
 
-	if isDebug {
-		log.Println("loadGoFiles got Struct count:", len(ises))
-	}
-
 	// generate
 	generateFile(ises, &outfileCfg{
-		exportFilename:  path.Join(uCfg.ExportDir, uCfg.ExportFilename),
-		exportPkgName:   uCfg.ExportPkgName,
-		modelImportPath: uCfg.ModelImportPath,
+		exportFilename:  path.Join(cfg.ExportDir, cfg.ExportFilename),
+		exportPkgName:   cfg.ExportPkgName,
+		modelImportPath: cfg.ModelImportPath,
 	})
 
 	return nil
@@ -62,18 +70,21 @@ func SetCustomParseTagFunc(f parseTagFunc) {
 }
 
 // Exported, and specified type
-func loadGoFiles(dir string, filenames ...string) ([]*innerStruct, error) {
-	newFilenames := []string{}
+func loadGoFiles(dir string, importPath string, filenames ...string) ([]*innerStruct, error) {
+	conf.Cwd = dir
+	conf.CreateFromFilenames(importPath, filenames...)
 
-	for _, filename := range filenames {
-		newFilenames = append(newFilenames, path.Join(dir, filename))
-	}
-
-	conf.CreateFromFilenames("", newFilenames...)
 	prog, err := conf.Load()
 	if err != nil {
 		log.Println("load program err:", err)
 		return nil, err
+	}
+
+	if isDebug {
+		log.Println("dir:", dir)
+		log.Println("importPath:", importPath)
+		log.Println("filename:", filenames)
+		// log.Println("len of prog imported", len(prog.Imported))
 	}
 
 	return loopProgramCreated(prog.Created), nil
@@ -86,33 +97,33 @@ func loadGoFiles(dir string, filenames ...string) ([]*innerStruct, error) {
 func loopProgramCreated(
 	created []*loader.PackageInfo,
 ) (innerStructs []*innerStruct) {
-
 	for _, pkgInfo := range created {
 		pkgName := pkgInfo.Pkg.Name()
 		defs := pkgInfo.Defs
 
-		// log.Println(pkgName)
-		// for astExpr, typAndVal := range typs {
-		// 	if !typAndVal.IsType() && typAndVal.IsBuiltin() {
-		// 		continue
-		// 	}
-		// 	log.Println(typAndVal, astExpr)
+		// imports := pkgInfo.Pkg.Imports()
+		// for _, imp := range imports {
+		// 	log.Println(imp.Path(), imp.Name())
 		// }
 
 		for indent, obj := range defs {
-
 			if !indent.IsExported() ||
 				obj == nil ||
 				!strings.HasSuffix(indent.Name, specifiedStructTypeSuffix) {
 				continue
 			}
 
-			// log.Println(indent, obj)
-			// log.Println(indent.Name, indent.String(), obj.String())
-			// log.Println(obj)
-			is := parseStructString(obj.String())
+			st, ok := obj.Type().Underlying().(*types.Struct)
+			if !ok {
+				log.Println("not a struct, skip this")
+				continue
+			}
+			is := new(innerStruct)
+
+			is.content = st.String()
 			is.pkgName = pkgName
-			is.pureName()
+			is.name = obj.Name()
+			is.fields = parseStructFields(st)
 
 			if isDebug {
 				log.Println("parse one Model: ", is.name, is.pkgName, is.content)
@@ -124,6 +135,12 @@ func loopProgramCreated(
 	return
 }
 
+type field struct {
+	name string
+	typ  string
+	tag  string
+}
+
 type innerStruct struct {
 	fields  []*field
 	content string
@@ -131,6 +148,24 @@ type innerStruct struct {
 	pkgName string
 }
 
+// parseStructFields parse fields
+func parseStructFields(st *types.Struct) []*field {
+	flds := make([]*field, 0, st.NumFields())
+
+	for i := 0; i < st.NumFields(); i++ {
+		fld := st.Field(i)
+		isField := new(field)
+
+		isField.name = fld.Name()
+		isField.tag = parseTag(st.Tag(i))
+		isField.typ = fld.Type().String()
+
+		flds = append(flds, isField)
+	}
+	return flds
+}
+
+/*
 func (is *innerStruct) pureName() {
 	is.name = strings.TrimPrefix(is.name, is.pkgName+".")
 }
@@ -155,11 +190,6 @@ func parseStructString(s string) *innerStruct {
 
 }
 
-type field struct {
-	name string
-	typ  string
-	tag  string
-}
 
 // s usually like this:
 // {Name string "gorm:\"colunm:name\""; Password string "gorm:\"column:password\""}
@@ -217,7 +247,7 @@ func cleanTag(tag string) string {
 	tag = strings.TrimPrefix(tag, `"`)
 	return tag
 }
-
+*/
 // input: gorm:"colunm:name"
 // output: gorm:column:name
 func defaultParseTagFunc(s string) string {
