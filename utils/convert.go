20366c4180b9593f6c6c9fbe769cc7f16bf4028b
yeqown <yeqown@gmail.com>
2018-12-17 11:09:50 +0800

    feat: utils convert struct field to lower

--- utils/convert.go
+++ utils/convert.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"fmt"
 	"reflect"
+	"strings"
 )
 
 // ConvertStructToMap convert struct value into map[string]interface{}
@@ -44,4 +46,45 @@ func ConvertUint8Slice2String(u8s []uint8) string {
 		bs = append(bs, byte(u))
 	}
 	return string(bs)
+}
+
+// ToLower convert struct(only) fields to lower string
+// if the field value is string type ~
+func ToLower(v interface{}) {
+	if !mustbePtr(v) {
+		fmt.Printf("ToLower param(%s) not type of pointer\n",
+			reflect.ValueOf(v).Kind().String())
+		return
+	}
+	// must be pointer
+	value := reflect.ValueOf(v).Elem()
+	if !typeEqual(value, reflect.Struct) {
+		fmt.Println("ToLower param not type of struct")
+		return
+	}
+
+	// range and toLower
+	for i := 0; i < value.NumField(); i++ {
+		field := value.Field(i)
+		switch field.Type().Kind() {
+		case reflect.String:
+			field.SetString(
+				strings.ToLower(field.String()),
+			)
+		case reflect.Ptr:
+			ToLower(field.Interface())
+		}
+	}
+}
+
+func mustbePtr(in interface{}) bool {
+	v := reflect.ValueOf(in)
+	if v.Kind() == reflect.Ptr {
+		return true
+	}
+	return false
+}
+
+func typeEqual(v reflect.Value, kind reflect.Kind) bool {
+	return v.Type().Kind() == kind
 }
--- utils/convert_test.go
+++ utils/convert_test.go
@@ -37,3 +37,107 @@ func TestConvertStructToMap(t *testing.T) {
 		})
 	}
 }
+
+func TestToLower(t *testing.T) {
+	type IStruct struct {
+		Field string
+		Slice []int
+	}
+	type Struct struct {
+		Ptr    *IStruct
+		IS     IStruct
+		FieldS string
+	}
+
+	type args struct {
+		v interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "case 1",
+			args: args{
+				v: &Struct{
+					Ptr:    &IStruct{"(*hj1bv232AAa", []int{123, 123123, 12312}},
+					IS:     IStruct{"HJKAJSBAK", []int{12312, 123, 1231, 231231}},
+					FieldS: "*&AGSIASBUAIS123123",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ToLower(tt.args.v)
+			t.Logf("result: %v", tt.args.v)
+			iv := tt.args.v.(*Struct)
+			t.Logf("result: %v", iv.Ptr)
+		})
+	}
+}
+
+func Test_mustbePtr(t *testing.T) {
+	ptr := new(int)
+	*ptr = 19
+	nonPtr := 20
+
+	type args struct {
+		in interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "case 1",
+			args: args{
+				in: ptr,
+			},
+			want: true,
+		},
+		{
+			name: "case 2",
+			args: args{
+				in: nonPtr,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mustbePtr(tt.args.in); got != tt.want {
+				t.Errorf("mustbePtr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_typeEqual(t *testing.T) {
+	type args struct {
+		v    reflect.Value
+		kind reflect.Kind
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "case 1",
+			args: args{
+				v:    reflect.ValueOf("askajhsk"),
+				kind: reflect.String,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := typeEqual(tt.args.v, tt.args.kind); got != tt.want {
+				t.Errorf("typeEqual() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
