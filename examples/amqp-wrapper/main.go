e36fa2f32d17694dc56e50f166b94818927cfbf2
yeqown <yeqown@gmail.com>
2019-10-10 17:02:12 +0800

    feat: finish produce

--- examples/amqp-wrapper/main.go
+++ examples/amqp-wrapper/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/streadway/amqp"
@@ -46,9 +47,23 @@ func main() {
 	w := amqpw.New(
 		"amqp://username:password@host:port",
 		amqp.Config{
-			Vhost:     "vhost",
+			Vhost:     "mq.class100.com",
 			Heartbeat: 2 * time.Second,
 		}, apply)
+
+	go func() {
+		ticker := time.NewTicker(3 * time.Second)
+		for {
+			select {
+			case <-ticker.C:
+				if err := w.Produce(ex, routingKey, false, false, []byte("hello")); err != nil {
+					log.Println("could not produce: ", err)
+				}
+			default:
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
 
 	go w.Consume(queue, "", false, false, false, false, nil, consume)
 
