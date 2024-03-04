95db35b14a7c7881061ceb03349a4d063fb71844
yeqown <yeqown@gmail.com>
2024-03-04 22:19:12 +0800

    feat: refactor consistent hash

--- framework/consistent-hashing/hash.go
+++ framework/consistent-hashing/hash.go
@@ -3,132 +3,176 @@ package chashing
 
 import (
 	"errors"
+	"fmt"
 	"hash"
-	"strings"
-
-	"github.com/yeqown/infrastructure/pkg/lang"
+	"hash/crc32"
+	"sort"
+	"sync"
 )
 
-const (
-	defaultMappingVirtualNodes = 100
-)
+type ConsistentHash interface {
+	AddNode(n Node) error
+	RemoveNode(n Node) error
 
-// ConsistentHashing .
-type ConsistentHashing struct {
-	nodesNum  int               // nodes count
-	vnodesNum int               // virtual nodes num
-	h         hash.Hash32       // hash32 algthrim
-	nodes     map[string]Node   // all actual nodes
-	vnodes    map[string]Node   // all virtual nodes
-	mapping   map[string]string // virtual to actual
-	// TODO:
+	HashKey(key []byte) Node
 }
 
-// NewConsistentHashing .
-func NewConsistentHashing(h hash.Hash32, nodes ...Node) (*ConsistentHashing, error) {
-	if len(nodes) == 0 {
-		return nil, errors.New("zero nodes to init")
+// Node represents a node in the consistent hashing ring.
+type Node interface {
+	Identity() []byte
+	Replicas() uint8
+}
+
+var _ ConsistentHash = (*ConsistentHashing)(nil)
+
+const (
+	defaultVirtualNodeReplicas = 100
+)
+
+type ConsistentHashing struct {
+	lock sync.RWMutex
+
+	hash32 hash.Hash32 // hash32 algorithm
+	nodes  []Node      // all actual nodes
+	// TODO: merge nodeIds and nodeReplicas
+	nodeIds       map[string]uint32 // all node identities for duplicated check; nodeId:nodeIdx
+	nodesReplicas map[uint32]uint8  // all actual nodes setting; nodeIdx: replicas
+	ring          []uint32          // ring of virtual nodes;    virtualNodeHash
+	ringMapping   map[uint32]uint32 // ring mapping to actual nodes; virtualNode: nodeIdx
+}
+
+// New constructs a ConsistentHashing instance to manage nodes and
+// locate key to target node.
+func New(h hash.Hash32, initialNodes ...Node) (*ConsistentHashing, error) {
+	if len(initialNodes) == 0 {
+		return nil, errors.New("zero initialNodes to init")
 	}
 	if h == nil {
-		// true: set default hash.Hash
-		// TODO:
-		// h =
-	}
-	ch := ConsistentHashing{
-		nodesNum: len(nodes),
-		h:        h,
-		nodes:    make(map[string]Node, len(nodes)),
-		vnodes:   make(map[string]Node, len(nodes)*defaultMappingVirtualNodes),
-		mapping:  make(map[string]string, len(nodes)*defaultMappingVirtualNodes),
+		h = crc32.NewIEEE()
 	}
 
-	for idx := range nodes {
-		if err := ch.AddNode(nodes[idx]); err != nil {
-			return nil, err
+	n := len(initialNodes)
+
+	ch := ConsistentHashing{
+		hash32:        h,
+		nodes:         make([]Node, 0, n),
+		nodeIds:       make(map[string]uint32),
+		nodesReplicas: make(map[uint32]uint8, n),
+		ring:          make([]uint32, 0, n*defaultVirtualNodeReplicas),
+		ringMapping:   make(map[uint32]uint32, n*defaultVirtualNodeReplicas),
+	}
+
+	for _, node := range initialNodes {
+		if err := ch.addNode(node, false); err != nil {
+			fmt.Printf("WARN: addNode(%s) failed: %v", node.Identity(), err.Error())
+			continue
 		}
 	}
+
+	// sort ring for binary search
+	sort.Slice(ch.ring, func(i, j int) bool {
+		return ch.ring[i] < ch.ring[j]
+	})
 
 	return &ch, nil
 }
 
-// Locate .
-func (ch *ConsistentHashing) Locate(key string) Node {
-	pos := ch.pos(key)
-	vnode := ch.locate(pos)
-	return ch.mappingTo(vnode)
-}
-
-// calc hash position in 2^32
-func (ch *ConsistentHashing) pos(key string) uint32 {
-	ch.h.Reset()
-	ch.h.Write(lang.StringToBytes(key))
-	return ch.h.Sum32()
-}
-
-// locate
-// TODO: pined same key into same node
-// FIXME: if part of nodes is Migrating, should stop locate to target nodes
-func (ch *ConsistentHashing) locate(pos uint32) Node {
-	return nil
-}
-
-// AddNode .
-// valid node; valid node ident duplicated;
-// FIXME: actual you should migrate data from last node of new node
 func (ch *ConsistentHashing) AddNode(n Node) error {
-	if err := validNode(n); err != nil {
-		return err
-	}
-
-	if _, ok := ch.nodes[n.Ident()]; ok {
-		return ErrDuplicatedNodeIdent
-	}
-
-	return ch.addNode(n)
+	return ch.addNode(n, true)
 }
 
-// add virtual nodes; add mapping
-func (ch *ConsistentHashing) addNode(n Node) error {
-	var (
-		vn    virtualNode
-		ident = n.Ident()
-	)
+func (ch *ConsistentHashing) addNode(node Node, doSort bool) error {
+	ch.lock.Lock()
+	defer ch.lock.Unlock()
 
-	ch.nodes[ident] = n
-
-	for i := 1; i <= defaultMappingVirtualNodes; i++ {
-		vn = newVirtualNode(ident, i)
-		ch.vnodes[vn.Ident()] = vn
-		ch.mapping[vn.Ident()] = ident
+	nodeId := node.Identity()
+	if _, ok := ch.nodeIds[string(nodeId)]; ok {
+		fmt.Println("WARN: duplicated node identity, skip it: ", string(nodeId))
+		return errors.New("duplicated node identity")
 	}
-	ch.vnodesNum += defaultMappingVirtualNodes
+
+	nodeIdx := len(ch.nodes)
+	ch.nodes = append(ch.nodes, node)
+
+	// record node identity
+	ch.nodeIds[string(nodeId)] = uint32(nodeIdx)
+
+	// decide replicas for each node by setting or default
+	replicas := node.Replicas()
+	if replicas == 0 {
+		replicas = defaultVirtualNodeReplicas
+	}
+	ch.nodesReplicas[uint32(nodeIdx)] = replicas
+
+	// generate virtual nodes and ring mapping
+	for i := uint8(0); i < replicas; i++ {
+		vNodeKey := append(nodeId, '@', '@', byte(i))
+		vNodeHash := crc32.ChecksumIEEE(vNodeKey)
+		ch.ring = append(ch.ring, vNodeHash)
+		ch.ringMapping[vNodeHash] = uint32(nodeIdx)
+	}
+
+	if doSort {
+		// sort ring for binary search
+		sort.Slice(ch.ring, func(i, j int) bool {
+			return ch.ring[i] < ch.ring[j]
+		})
+	}
 
 	return nil
 }
 
-// RemoveNode .
-// FIXME: actual you should migrate data from removed node
 func (ch *ConsistentHashing) RemoveNode(n Node) error {
-	var ident = n.Ident()
-	if _, ok := ch.nodes[ident]; !ok {
-		return ErrNodeNotFound
+	ch.lock.Lock()
+	defer ch.lock.Unlock()
+
+	nodeId := n.Identity()
+	if _, ok := ch.nodeIds[string(nodeId)]; !ok {
+		return errors.New("node not found")
 	}
 
-	delete(ch.nodes, ident)
+	nodeIdx := ch.nodeIds[string(nodeId)]
 
-	for vIdent, aIdent := range ch.mapping {
-		if strings.Compare(aIdent, ident) == 0 {
-			// trur: equal
-			delete(ch.vnodes, vIdent)
-			delete(ch.mapping, vIdent)
+	// remove actual node from nodes
+	ch.nodes = append(ch.nodes[:nodeIdx], ch.nodes[nodeIdx+1:]...)
+	// remove node identity from nodeIds
+	delete(ch.nodeIds, string(nodeId))
+	// remove replicas setting for each node by setting or default
+	replicas := ch.nodesReplicas[nodeIdx]
+	delete(ch.nodesReplicas, nodeIdx)
+
+	// remove virtual nodes and ring mapping
+	newRing := make([]uint32, 0, len(ch.ring)-int(replicas))
+	for i := 0; i < len(ch.ring); i++ {
+		if ch.ringMapping[ch.ring[i]] != nodeIdx {
+			// keep the virtual node if it's not belong to the removed node
+			newRing = append(newRing, ch.ring[i])
+			continue
 		}
+
+		// remove the virtual node from ringMapping
+		delete(ch.ringMapping, ch.ring[i])
 	}
 
 	return nil
 }
 
-// mapping from virtual node to actual node
-func (ch *ConsistentHashing) mappingTo(vNode Node) Node {
-	id := ch.mapping[vNode.Ident()]
-	return ch.nodes[id]
+func (ch *ConsistentHashing) HashKey(key []byte) Node {
+	h := crc32.ChecksumIEEE(key)
+	ch.lock.RLock()
+	defer ch.lock.RUnlock()
+
+	// binary search to locate the first node that >= h
+	idx := sort.Search(len(ch.ring), func(i int) bool {
+		return ch.ring[i] >= h
+	})
+
+	// if idx == len(ch.ring), then idx = 0
+	if idx == len(ch.ring) {
+		idx = 0
+	}
+
+	// get the actual node index
+	nodeIdx := ch.ringMapping[ch.ring[idx]]
+	return ch.nodes[nodeIdx]
 }
